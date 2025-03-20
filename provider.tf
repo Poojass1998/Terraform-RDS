2b7feb1166c74ee7424ad6307dcea9ed2db84ab4
Pooja S S <poojass423@gmail.com>
2025-03-20 22:36:09 +0530

    Initial Terraform RDS setup

--- provider.tf
+++ provider.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucet"  
-    key            = "terraform/terraform.tfstate"
-    region         = "ap-south-1"
-    encrypt        = true
-    dynamodb_table = "terraform-state-lock"   
+    bucket       = "terraform-state-bucet"
+    key          = "terraform/terraform.tfstate"
+    region       = "ap-south-1"
+    encrypt      = true
+    use_lockfile = true 
   }
 }
 
