6ac9ff4b5df8eaccf915001e4c62ae3fd85b9cc5
Pooja S S <poojass423@gmail.com>
2025-03-21 07:56:40 +0530

    updated rds.tf

--- rds.tf
+++ rds.tf
@@ -1,15 +1,14 @@
 resource "aws_db_instance" "employee_rds" {
   allocated_storage    = 20
   storage_type         = "gp2"
-  engine              = "mysql"  # Change to "postgres" if needed
-  engine_version      = "8.0"   # Update based on your requirement
+  engine              = "mysql" 
+  engine_version      = "8.0"   
   instance_class      = "db.t3.micro"
   identifier         = "employee-db"
   username          = "admin"
   password          = "poojards"
   publicly_accessible = false
   skip_final_snapshot = true
-  multi_az = false
   
   vpc_security_group_ids = [aws_security_group.db_sg.id]
   db_subnet_group_name   = aws_db_subnet_group.employee_db_subnet.name
